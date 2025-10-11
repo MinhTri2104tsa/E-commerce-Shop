2b26394395ad7d964345160eaee6e3565c7e8468
MinhTri <mtri2105@gmail.com>
2025-10-11 22:24:48 +0700

    Add route, component basic of web

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,27 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/slides/counterSlide'
-import { styled } from 'styled-components';
-
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {routes} from './routes'
+import DefaultComponent from './components/DefaultComponent/DefaultComponent';
 export function App() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
-
-  const Button = styled.button({
-    background: 'blue',
-  })
+  
 
   return (
     <div>
-      <div>
-        <Button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </Button>
-        <span>{count}</span>
-        <Button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </Button>
-      </div>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page;
+            const Layout  = route.isShowHeader ? DefaultComponent : React.Fragment;
+            return (
+              <Route key={route.path} path={route.path} element={
+                 <Layout>
+                  <Page />
+                 </Layout>
+              } />
+            );
+          })}
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      okey
+    </div>
+  )
+}
+
+export default HomePage
