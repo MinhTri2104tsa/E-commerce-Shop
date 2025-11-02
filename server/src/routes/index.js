0fa79f9e2c04787105879fa665798d97e52e077a
Nguyen Minh Tri <mtri2105@gmail.com>
2025-11-02 23:05:18 +0700

    setup code CRUD product

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,14 +1,9 @@
-const express = require('express');
-const UserController = require('../controllers/UserController');
-const router = express.Router();
-
-router.post('/', UserController.createUser);
+const UserRouter = require('./UserRouter');
+const ProductRouter = require('./ProductRouter')
 
 const routes = (app) => {
-    app.use('/api/user', router);
-    app.use('/api/user/sign-up', router);
-    app.use('/api/user/sign-in', router);
-    app.use('/api/user/update-user', router);
+    app.use('/api/user', UserRouter);
+    app.use('/api/product', ProductRouter);
 };
 
 module.exports = routes;
